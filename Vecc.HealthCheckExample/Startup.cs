5b44b6416dcf7765e046c3dec1ed09ef2401dcc9
Edward Cooke <edward@frakkingsweet.com>
2020-08-13 14:48:35 -0600

    Added basic health checks

--- Vecc.HealthCheckExample/Startup.cs
+++ Vecc.HealthCheckExample/Startup.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 
 namespace Vecc.HealthCheckExample
@@ -23,6 +25,11 @@ namespace Vecc.HealthCheckExample
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddHealthChecks()
+                .AddCheck("ping", () => new HealthCheckResult(HealthStatus.Healthy, "pong"), new string[] { "ping" })
+                .AddCheck("remote1", () => new HealthCheckResult(HealthStatus.Healthy, "always healthy"), new string[] { "remote" })
+                .AddCheck("remote2", () => new HealthCheckResult(HealthStatus.Degraded, "always degraded"), new string[] { "remote" })
+                .AddCheck("remote3", () => new HealthCheckResult(HealthStatus.Unhealthy, "always unhealthy"), new string[] { "remote" });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +51,14 @@ namespace Vecc.HealthCheckExample
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapHealthChecks("/hc/ping", new HealthCheckOptions
+                {
+                    Predicate = check => check.Tags.Contains("ping")
+                });
+                endpoints.MapHealthChecks("/hc/remote", new HealthCheckOptions
+                {
+                    Predicate = check => check.Tags.Contains("remote")
+                });
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
--- readme.md
+++ readme.md
@@ -1,4 +1,8 @@
-# Companion post:
-
 # Description
-Empty Core 3.1 project with no modifications
+Add basic static, out of the box, health checks. These are nothing fancy.
+
+This only returns the final state, they don't return individual health check results.
+
+# Urls
+* `/hc/ping`
+* `/hc/remote`
